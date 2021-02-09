359d9135361386da7de83bdb0ce958420f12bede
pudu87 <sam.cappelle87@gmail.com>
2021-02-09 16:09:39 -0300

    add data object for react state

---
+++ src/logic/data.js
@@ -0,0 +1,41 @@
+import Ship from './Ship'
+
+const player = {
+  board: Array(10).fill().map(() => Array(10).fill(false)),
+  ships: {
+    carrier: { 
+      ...Ship('carrier', 5), 
+      hits: 0 
+    },
+    battleship: { 
+      ...Ship('battleship', 4), 
+      hits: 0 
+    },
+    cruiser: { 
+      ...Ship('cruiser', 3), 
+      hits: 0 
+    },
+    submarine: { 
+      ...Ship('submarine', 3), 
+      hits: 0 
+    },
+    minesweeper: { 
+      ...Ship('minesweeper', 2), 
+      hits: 0 
+    },
+  }, 
+  history: []
+}
+
+const data = {
+  setupComplete: false,
+  gameOver: false,
+  human: {
+    ...player
+  },
+  computer: {
+    ...player
+  }
+}
+
+export default data;
